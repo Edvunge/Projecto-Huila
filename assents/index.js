b5232d6b85109c0a6ba43b7e5a947260bfe9f663
edvunge <edvaldolaurentina07@hotmail.com>
2021-07-30 16:51:41 +0100

    11th- Eleventh - alteraçao da galeria

--- assents/index.js
+++ assents/index.js
@@ -31,3 +31,15 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+/*---------------*/
+
+// Toggle grid padding
+function myFunction() {
+  var x = document.getElementById("myGrid");
+  if (x.className === "w3-row") {
+    x.className = "w3-row-padding";
+  } else { 
+    x.className = x.className.replace("w3-row-padding", "w3-row");
+  }
+}
